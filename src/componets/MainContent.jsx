f6bd808d3c0388678ce34717700b37187cdb170e
SteelyxYT <91285184+SteelyxYT@users.noreply.github.com>
2024-08-22 10:16:12 +0200

    removed list

--- src/componets/MainContent.jsx
+++ src/componets/MainContent.jsx
@@ -2,12 +2,10 @@
 export default function MainContent() {
     return (
         <div className="main-content">
-            <ul>
-                <li><details><summary>interesser</summary><div></div></details></li>
-                <li><details><summary>Programmering</summary><div></div></details></li>
-                <li><details><summary>Gaming</summary><div></div></details></li>
-                <li><details><summary>Projekter</summary><div></div></details></li>
-            </ul>
+                <details><summary>interesser</summary><div></div></details>
+                <details><summary>Programmering</summary><div></div></details>
+                <details><summary>Gaming</summary><div></div></details>
+                <details><summary>Projekter</summary><div></div></details>
         </div>
     )
 }
