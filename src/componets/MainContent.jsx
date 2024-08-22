ae558357920c7ee44c4914e8c3ff05ca0e7aa10c
SteelyxYT <91285184+SteelyxYT@users.noreply.github.com>
2024-08-22 10:34:30 +0200

    added content structure

--- src/componets/MainContent.jsx
+++ src/componets/MainContent.jsx
@@ -5,6 +5,14 @@ export default function MainContent() {
         <summary>interesser</summary>
         <div>
             <h3>Mine interesser:</h3>
+            <div className="programContent">
+                <h4>Programmering:</h4>
+                <p></p>
+                </div>
+                <div className="gamingContent">
+                    <h4>Gaming:</h4>
+                    <p></p>
+                </div>
         </div>
       </details>
       <details>
